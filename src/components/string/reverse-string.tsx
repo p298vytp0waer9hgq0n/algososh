404fa2ec0783564b221aca32b9faf8977c70c208
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-02-13 18:54:05 +0500

    feat: add word display to string page

---
+++ src/components/string/reverse-string.tsx
@@ -0,0 +1,17 @@
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+
+import styles from "./string.module.css";
+
+type TReverseStringProps = {
+    word: {letter: string; state: ElementStates}[];
+}
+
+export function ReverseString ({ word }: TReverseStringProps) {
+    const elements = word.map(({ letter, state }, index) => {
+        return <Circle letter={letter} state={state} key={index} />;
+    })
+    return (
+        <div className={`${styles.letters} ${styles.visible}`}>{elements}</div>
+    )
+}
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,17 +4,27 @@ import { Button } from "../ui/button/button";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 import styles from "./string.module.css";
+import { ReverseString } from "./reverse-string";
+import { ElementStates } from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
-  const [string, setString] = useState<string>();
+  const [string, setString] = useState<string>('');
   const [working, setWorking] = useState(false);
+  const [workingWord, setWorkingWord] = useState<any>();
   const strInput = useRef<HTMLInputElement>(null);
   
   function handleClick () {
     setWorking(true);
+    const word = Array.from(string).map((letter) => {
+      return {letter: letter, state: ElementStates.Default};
+    })
+    setWorkingWord(word);
   }
   function handleChange (evt: React.ChangeEvent<HTMLInputElement>) {
     setString(evt.target.value);
+  }
+  function finishRender () {
+    setWorking(false);
   }
 
   return (
@@ -23,6 +33,7 @@ export const StringComponent: React.FC = () => {
         <Input ref={strInput} isLimitText={true} maxLength={11} value={string} onChange={handleChange} />
         <Button onClick={handleClick} isLoader={working}>Развернуть</Button>
       </div>
+      { workingWord && <ReverseString word={workingWord} /> }
     </SolutionLayout>
   );
 };
