ee20753935cb243160d60107c7f3de65f6065ee9
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-03-02 14:19:34 +0500

    feat: queue page logic

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import useQueue from "../../hooks/use-queue";
+import { ElementStates } from "../../types/element-states";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import styles from "./queue-page.module.css";
+
+const queueLength = 7;
+enum RunningValues {
+  Enqueue = 1,
+  Dequeue,
+  Clear
+}
+
 export const QueuePage: React.FC = () => {
+  const [value, setValue] = useState<string>();
+  const [running, setRunning] = useState<RunningValues | false>(false);
+  const queue = useQueue<string>(queueLength);
+  
+  function enqueue () {
+    queue.enqueue(value as string);
+    setRunning(RunningValues.Enqueue);
+    setValue('');
+    setTimeout(() => {
+      setRunning(false);
+    }, SHORT_DELAY_IN_MS);
+  }
+  function dequeue () {
+    setRunning(RunningValues.Dequeue);
+    setTimeout(() => {
+      queue.dequeue();
+      setRunning(false);
+    }, SHORT_DELAY_IN_MS);
+  }
+  function clearQueue () {
+    setRunning(RunningValues.Clear);
+    setTimeout(() => {
+      queue.clear();
+      setRunning(false);
+    }, SHORT_DELAY_IN_MS);
+  }
+
+  const elements = queue.elements.map((ele, index) => {
+    const isHead = !queue.isEmpty && index === queue.headIndex;
+    const isTail = !queue.isEmpty && index === queue.tailIndex;
+    let styleValue = ElementStates.Default;
+    if ((running === RunningValues.Enqueue && isTail) || (running === RunningValues.Dequeue && isHead) || (running === RunningValues.Clear && ele)) styleValue = ElementStates.Changing; 
+    return (
+      <Circle 
+        key={index}
+        letter={ele}
+        index={index}
+        head={isHead ? 'head' : undefined}
+        tail={isTail ? 'tail' : undefined}
+        state={styleValue}
+      />
+    )
+  })
+
   return (
     <SolutionLayout title="Очередь">
-
+      <div className={styles.container}>
+        <Input value={value || ''} onChange={(evt: React.ChangeEvent<HTMLInputElement>) => setValue(evt.target.value)} maxLength={4} isLimitText={true} />
+        <Button onClick={enqueue}>Добавить</Button>
+        <Button onClick={dequeue}>Удалить</Button>
+        <Button onClick={clearQueue}>Очистить</Button>
+      </div>
+      <div className={styles.render}>
+        {elements}
+      </div>
     </SolutionLayout>
   );
 };
---
+++ src/utils/queue.ts
@@ -0,0 +1,73 @@
+export type TQueue<T> = {
+    enqueue: (arg0: T) => void;
+    dequeue: () => void;
+    clear: () => void;
+    peek: () => T | undefined;
+    isEmpty: boolean;
+    headIndex: number;
+    tailIndex: number;
+    elements: Array<T | undefined>;
+}
+
+export default class Queue<T> implements TQueue<T> {
+    private container: Array<T | undefined> = [];
+    private head: number = 0;
+    private tail: number = 0;
+    private size: number = 0;
+    private length: number = 0;
+
+    constructor (length: number) {
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+        this.container = Array(length);
+        this.length = length;
+    }
+    
+    enqueue (item: T) {
+        if (this.size === this.length) {
+            throw new Error("The queue is full, yet additional enqueue attempted.")
+        }
+        this.container[this.tail % this.length] = item;
+        this.tail++;
+        this.size++;
+    }
+
+    dequeue () {
+        if (this.size === 0) {
+            throw new Error("The queue is empty, yet dequeue attempted.")
+        }
+        this.container[this.head % this.length] = undefined;
+        this.head++;
+        this.size--;
+    }
+    
+    clear () {
+        for (let i = 0; i < this.length; i++) {
+            this.container[i] = undefined;
+        }
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+    
+    peek () {
+        return this.container[this.head];
+    }
+
+    get isEmpty () {
+        return (this.size === 0);
+    }
+    
+    get headIndex () {
+        return this.head % this.length;
+    }
+
+    get tailIndex () {
+        return Math.max(this.tail - 1, 0) % this.length;
+    }
+
+    get elements () {
+        return [...this.container];
+    }
+}
